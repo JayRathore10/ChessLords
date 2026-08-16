58596294d99c2436d791d0b56cf7078dae315793
Jay <jayrathore88155@gmail.com>
2026-08-16 20:10:25 +0530

    feat: Add dotenv and cors also the FRONTEND env variable

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,16 @@
 import express  , {Request , Response} from "express";
+import cors from "cors";
+import { FRONTEND } from "./configs/env.config";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: FRONTEND , 
+  methods: ["GET", "POST" , "DELETE" , "PUT"],
+  credentials: true
+}));
+
 
 app.get("/"  , (req : Request, res : Response)=>{
   res.send("Hi, Jexts here!")
--- server/tests/example.test.ts
+++
@@ -1,9 +0,0 @@
-describe("Example Test", () => {
-
-  test("should work correctly", () => {
-
-    expect(true).toBe(true);
-
-  });
-
-});
